36a531adca47c9e1449f569dfb953de320e63d41
Mitchell Michalak <38023623+mmichalak-swe@users.noreply.github.com>
2023-12-01 19:23:04 -0500

    git init, Day 1: p1 & p2

---
+++ Day1_p1.py
@@ -0,0 +1,21 @@
+output = 0
+
+
+with open('./Day1_Input.txt','r', encoding="utf-8") as myfile:
+    for line in myfile:
+        first_digit = ''
+        second_digit = ''
+
+        for char in line:
+            if char.isdigit():
+                first_digit = char
+                break
+
+        for char in line[::-1]:
+            if char.isdigit():
+                second_digit = char
+                break
+
+        output += (int(first_digit) * 10) + int(second_digit)
+
+print(output)
---
+++ Day1_p2.py
@@ -0,0 +1,70 @@
+output = 0
+
+trie = {"o": {"on": {"one": '!'}},
+        "t": {"tw": {"two": '!'},
+              "th": {"thr": {"thre": {"three": '!'}}}},
+        "f": {"fo": {"fou": {"four": '!'}},
+              "fi": {"fiv": {"five": '!'}}},
+        "s": {"si": {"six": '!'},
+              "se": {"sev": {"seve": {"seven": '!'}}}},
+        "e": {"ei": {"eig": {"eigh": {"eight": '!'}}}},
+        "n": {"ni": {"nin": {"nine": '!'}}}}
+
+word_to_number = {"one": 1,
+                  "two": 2,
+                  "three": 3,
+                  "four": 4,
+                  "five": 5,
+                  "six": 6,
+                  "seven": 7,
+                  "eight": 8,
+                  "nine": 9
+}
+
+
+def word_checker(lookup, test_str, idx=1):
+    if idx > len(test_str):
+        return -1
+
+    curr = test_str[:idx]
+    if curr not in lookup:
+        return -1
+
+    if lookup[curr] == '!':
+        return curr
+
+    return word_checker(lookup[curr], test_str, idx+1)
+
+
+with open('./Day1_Input.txt','r', encoding="utf-8") as myfile:
+    for line in myfile:
+        line = line.strip()
+        first_digit = ''
+        second_digit = ''
+
+        for i, char in enumerate(line):
+            if char.isdigit():
+                first_digit = char
+                break
+            if char in trie:
+                check_str = line[i:i+5]
+                result = word_checker(trie, check_str)
+                if result != -1:
+                    first_digit = word_to_number[result]
+                    break
+
+        for i in range(len(line) - 1, -1, -1):
+            char = line[i]
+            if char.isdigit():
+                second_digit = char
+                break
+            if char in trie:
+                check_str = line[i:i+5]
+                result = word_checker(trie, check_str)
+                if result != -1:
+                    second_digit = word_to_number[result]
+                    break
+
+        output += (int(first_digit) * 10) + int(second_digit)
+
+print(output)
