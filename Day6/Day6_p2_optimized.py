89e4f1dfe0ea2676a43e62240171134bd510e900
Mitchell Michalak <38023623+mmichalak-swe@users.noreply.github.com>
2023-12-07 01:18:01 -0500

    Day6: optimized p2

---
+++ Day6/Day6_p2_optimized.py
@@ -0,0 +1,10 @@
+with open("./Day6/Day6_Input.txt",'r', encoding="utf-8") as file:
+    file_parse = file.read().split('\n')
+
+time = int(''.join(file_parse[0].split(': ')[1].strip(' ').split()))
+record = int(''.join(file_parse[1].split(': ')[1].strip(' ').split()))
+
+d = (time**2 - (4 * record))**0.5
+x1 = (time - (d))/(2)
+x2 = (time + (d))/(2)
+print(int(x2)-int(x1))
