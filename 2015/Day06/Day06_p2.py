765d1ed39e469a737196ab1809f793f8821efb29
Mitchell Michalak <38023623+mmichalak-swe@users.noreply.github.com>
2023-12-18 21:28:48 -0500

    Day06: p2

---
+++ 2015/Day06/Day06_p2.py
@@ -0,0 +1,39 @@
+with open("./Day06/Day06_Input.txt",'r', encoding="utf-8") as file:
+    file_parse = file.read().split('\n')
+
+def change_state(d, start, end, action):
+    start_row, end_row = start[0], end[0]
+    start_col, end_col = start[1], end[1]
+
+    for i in range (start_row, end_row + 1):
+        for j in range(start_col, end_col + 1):
+            if action == 'toggle':
+                d[(i, j)] = d.get((i, j), 0) + 2
+            elif action == 'on':
+                d[(i, j)] = d.get((i, j), 0) + 1
+            elif action == 'off':
+                d[(i, j)] = d.get((i, j), 0) - 1
+                d[(i, j)] = 0 if d[(i, j)] < 0 else d[(i, j)]
+
+
+lights = {}
+
+for i in range(999):
+    for j in range(999):
+        lights[(i, j)] = 0
+
+for line in file_parse:
+    instruction = line.split(' ')
+    action = 'toggle' if instruction[0] == 'toggle' else ''
+    if not action:
+        action = 'on' if instruction[1] == 'on' else 'off'
+
+    start_str = instruction[-3].split(',')
+    end_str = instruction[-1].split(',')
+
+    start = (int(start_str[0]), int(start_str[1]))
+    end = (int(end_str[0]), int(end_str[1]))
+
+    change_state(lights, start, end, action)
+
+print(sum(lights.values()))
